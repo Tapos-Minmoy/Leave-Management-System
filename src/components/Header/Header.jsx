35207776adc6fb085f81215209e0f8aaf5919190
Saima-Amin <saimaamin4th2022@gmail.com>
2024-02-17 02:26:56 +0600

    name commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <Navbar fluid rounded>
           <Navbar.Brand href="https://flowbite-react.com">
             <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">University of Chittagong</span>
           </Navbar.Brand>
           <div className="flex md:order-2">
             <Dropdown
