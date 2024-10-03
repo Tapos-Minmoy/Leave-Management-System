85ad47bddfbe4370059d2895ae57d6a3d08a6197
souravkcoder <souravk.coder@gmail.com>
2024-10-03 11:25:12 +0600

    CORS policy added

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    cors: {
+      origin: '*', // Allows all origins, you can specify specific origins like 'https://example.com'
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specifies allowed methods
+      
+      credentials: true, // If you want to allow cookies or authorization headers
+    }
+  }
 })
